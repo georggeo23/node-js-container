02a86d879cca93b9a6d78ffb35837c4e33552610
georggeo23 <georgechris2003@gmail.com>
2024-12-06 16:14:42 -0500

    updated file commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
-  res.send("Hello hello hello hello, Dockerized Node.js App!");
+  res.send("Dockerized Node.js App!");
 });
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`);
